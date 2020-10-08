9164e3fa0d00bb9d69084da7cf2a341aac020198
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-10-08 21:25:32 +0600

    header update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
             <nav className="navbar navbar-expand-lg fixed-top">
                 <div className="container">
                     <Link to="/" className="navbar-brand">
-                        <img src={logo} alt="" className="img-fluid logo"/>
+                        Doctors <strong>Portal</strong>
                     </Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
