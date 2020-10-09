53e0af491d5de5918f05a6297825324c201372e3
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-10-10 00:01:37 +0600

    homepage update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
+        <div className="home-wrapper">
             <h1>This is home page!!!</h1>
         </div>
     );
