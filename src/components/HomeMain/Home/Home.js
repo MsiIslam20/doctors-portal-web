1eea24f593d8fac0b55cefbc87469e44c74777bb
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-10-12 19:40:51 +0600

    home update

--- src/components/HomeMain/Home/Home.js
+++ src/components/HomeMain/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import bg from '../../../images/home.png'
 import './Home.css';
 
 const Home = () => {
@@ -8,7 +9,16 @@ const Home = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-md-12 px-0">
-
+                        <div className="row">
+                            <div className="col-md-5">
+                                <h1>Your New Smile <br/> Starts Here</h1>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the</p>
+                                <button className="btn btn-primary btn-brand">Get Appointment</button>
+                            </div>
+                            <div className="col-md-7">
+                                <img src={bg} alt="" className="img-fluid"/>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
