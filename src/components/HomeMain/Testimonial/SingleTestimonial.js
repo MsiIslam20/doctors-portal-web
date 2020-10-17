03a6a36a8d18276226e3d38ef066a55472462e20
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-10-17 20:33:36 +0600

    updates

--- src/components/HomeMain/Testimonial/SingleTestimonial.js
+++ src/components/HomeMain/Testimonial/SingleTestimonial.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SingleTestimonial = ({testimonial}) => {
     return (
         <div>
-            
+            <h1>{testimonial.name}</h1>
         </div>
     );
 };
