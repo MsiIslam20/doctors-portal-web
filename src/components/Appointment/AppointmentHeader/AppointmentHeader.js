6b34a22112da5a2f7f52ec32fa65b1861a4f89cc
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-10-21 11:49:58 +0600

    appoinment page add

---
+++ src/components/Appointment/AppointmentHeader/AppointmentHeader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './AppoinmentHeader.css';
+
+const AppointmentHeader = () => {
+    return (
+        <section className="appointmentHeader-wrapper">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default AppointmentHeader;
