ae511519a647e5e3e9331eb95db17b625865093b
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-10-23 23:05:59 +0600

    updates

--- src/components/Appointment/BookAppointment/BookAppointment.js
+++ src/components/Appointment/BookAppointment/BookAppointment.js
@@ -7,35 +7,35 @@ const bookingData = [
         id: 1,
         subject: 'Teeth Orthodontics',
         visitingHour: '8:00 AM - 9:00 AM',
-        totalSpace: 10
+        totalSpace: 12
     },
     {
         _id: '5e8df578e6e8231764dc23df',
         id: 2,
         subject: 'Cosmetic Dentistry',
         visitingHour: '10:50 AM - 11:30 AM',
-        totalSpace: 10
+        totalSpace: 14
     },
     {
         _id: '5e8df5aee6e8231764dc23e0',
         id: 3,
         subject: 'Teeth Cleaning',
         visitingHour: '5:00 PM - 6:00 PM',
-        totalSpace: 10
+        totalSpace: 15
     },
     {
         _id: '5e8df63be6e8231764dc23e1',
         id: 4,
         subject: 'Cavity Protection',
         visitingHour: '7:00 AM - 8:30 AM',
-        totalSpace: 10
+        totalSpace: 11
     },
     {
         _id: '5e8df68de6e8231764dc23e2',
         id: 5,
         subject: 'Teeth Orthodontics',
         visitingHour: '8:00 AM - 9:00 AM',
-        totalSpace: 10
+        totalSpace: 12
     },
     {
         _id: '5e8df6a0e6e8231764dc23e3',
@@ -48,7 +48,7 @@ const bookingData = [
 
 const BookAppointment = ({date}) => {
     return (
-        <section className="bookAppointment-wrapper mt-5 pt-4">
+        <section className="bookAppointment-wrapper mt-5 pt-3">
             <div className="container">
                 <div className="row">
                     <div className="col-md-12 px-auto">
